23deb19fa2b8235fc1cf079094d2f0219bbbfaad
Sajib Adhikary <tosajibadhi@gmail.com>
2023-09-09 16:55:00 +0600

    upd: typeDefs
    Signed-off-by: Sajib Adhikary <tosajibadhi@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
   }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
+
 `
 
 // 2
